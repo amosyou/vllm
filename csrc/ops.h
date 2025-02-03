3cabb5a235057867ae4e197dc30ae042ef774531
Amos You <amosyou@berkeley.edu>
2025-02-03 10:01:01 +0000

    change function definition

--- csrc/ops.h
+++ csrc/ops.h
@@ -131,11 +131,13 @@ void gptq_shuffle(
 
 void moe_align_block_size(
   torch::Tensor topk_ids,
-  int num_experts,
-  int block_size,
+  int64_t num_experts,
+  int64_t block_size,
   torch::Tensor sorted_token_ids,
   torch::Tensor experts_ids,
   torch::Tensor num_tokens_post_pad);
+
+void moe_sum(torch::Tensor& input, torch::Tensor& output);
 
 #ifndef USE_ROCM
 using fptr_t = uint64_t;
